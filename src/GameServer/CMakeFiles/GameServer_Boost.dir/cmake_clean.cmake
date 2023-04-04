df22bb18d2ab81a2b0595d63036f84d514973f5d
root <root@build-machine.c0g0t4ythbdu1bjixbe5smqf4a.syx.internal.cloudapp.net>
2023-04-04 02:37:15 +0000

    this is test commit

---
+++ src/GameServer/CMakeFiles/GameServer_Boost.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameServer_Boost.dir/src/ClientPacketHandler.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Base/ClientBase.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Base/RoomBase.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/ClientManager.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Game/GameClient.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Game/GameObject.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Game/GameRoom.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Game/Scene.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Matching/MatchingClient.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Matching/MatchingRoom.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/MyRoom/MyRoomRoom.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Office/OfficeClient.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/Office/OfficeRoom.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/GameContents/RoomManager.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/HTTP/HttpServer.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Main/GameServer.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_000_Common.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_001_Base.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_002_Interaction.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_003_Myroom.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_004_Office.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Protocol/Packet_005_Matching.pb.cc.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Session/GameSession.cpp.o"
+  "CMakeFiles/GameServer_Boost.dir/src/Util/pch.cpp.o"
+  "GameServer_Boost"
+  "GameServer_Boost.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameServer_Boost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/ServerCore/CMakeFiles/ServerCore.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Job/GlobalQueue.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Job/GlobalQueue.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Job/Job.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Job/Job.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Job/JobQueue.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Job/JobQueue.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Job/JobTimer.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Job/JobTimer.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Job/LockQueue.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Job/LockQueue.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Main/Singleton.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Main/Singleton.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Network/Acceptor.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Network/Acceptor.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Network/RecvBuffer.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Network/RecvBuffer.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Network/SendBuffer.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Network/SendBuffer.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Network/Service.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Network/Service.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Network/Session.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Network/Session.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Thread/TLS.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Thread/TLS.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Thread/ThreadManager.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Thread/ThreadManager.cpp.o"
+  "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/src/Util/LogManager.cpp" "/home/lhg/arzMETA-GAME-SERVER-BOOST-MANY_ROOM/src/ServerCore/CMakeFiles/ServerCore.dir/src/Util/LogManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_THREAD_DYN_LINK"
+  "BOOST_THREAD_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
